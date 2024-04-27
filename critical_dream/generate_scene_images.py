2f6d695eedefadf0b451b3b51c7969e47e380e9a
cosmicBboy <niels.bantilan@gmail.com>
2024-04-26 22:52:25 -0400

    update prompts
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- critical_dream/generate_scene_images.py
+++ critical_dream/generate_scene_images.py
@@ -204,10 +204,10 @@ def add_prompts(scene: dict) -> dict:
         if addtl_prompts:
             full_character_desc = f"{full_character_desc}, {addtl_prompts}"
         prompt = (
-            f"({full_character_desc})+++ "
+            f"{full_character_desc} "
             f"{scene['action']}, "
             f"{scene['poses']}. "
-            f"({scene['background']} background, fantasy world)+++ ."
+            f"{scene['background']} background, fantasy world."
             f"{PROMPT_AUGMENTATION}"
         )
     else:
